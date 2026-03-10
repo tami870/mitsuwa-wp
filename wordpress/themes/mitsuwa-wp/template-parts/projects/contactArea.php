f907b63f55f3d73ba50610c595696706e21ab4da
foxx <heisei030809@gmail.com>
2026-03-10 16:51:41 +0900

    コンタクトエリア　CSS完了

--- wordpress/themes/mitsuwa-wp/template-parts/projects/contactArea.php
+++ wordpress/themes/mitsuwa-wp/template-parts/projects/contactArea.php
@@ -1,24 +1,25 @@
-<div class="p-contactArea">
+<section class="p-contactArea">
   <div class="l-inner">
     <div class="p-contactArea__header">
       <?php
       get_template_part("template-parts/compornet/sectionTitle", null, [
-        "sub_title" => "サービス",
+        "sub_title" => "お問い合わせ",
         "main_title" => "Contact",
         "heading_level" => "h2"
       ]);
       ?>
-      <strong class="p-contactArea__header-text u-sp-only">リフォームやガス機器に
-        関するご相談などお気軽
-        にお問合せください。</strong>
+      <div class="p-contactArea__header-text u-sp-only">リフォームやガス機器に<br>関するご相談などお気軽<br>にお問合せください。</div>
       <p class="p-contactArea__header-reception-time-text u-sp-only">
-        受付 9:00〜18:00(土・日・祝17:00まで)<br>
+        受付 9:00〜18:00<br>(土・日・祝17:00まで)<br>
         携帯電話からでも<br>
         フリーダイヤルへつながります
       </p>
-      <p class="p-contactArea__header-reception-time-text u-pc-only">受付 9:00〜18:00(土・日・祝17:00まで)
+      <p class="p-contactArea__header-reception-time-text u-pc-only">受付 9:00〜18:00(土・日・祝17:00まで)<br>
         携帯電話からでもフリーダイヤルへつながります。</p>
-      <?php get_template_part("template-parts/compornet/more-btn--big"); ?>
+      <div class="p-contactArea__header-btn">
+        <?php get_template_part("template-parts/compornet/more-btn--big"); ?>
+
+      </div>
     </div>
     <strong class="p-contactArea__header-text u-pc-only">リフォームやガス機器に関するご相談などお気軽にお問合せください。</strong>
     <div class="p-contactArea__cards">
@@ -27,11 +28,40 @@
         <a href="tel:075-761-4343" class="p-contactArea__card-tel">075-761-4343</a>
         <div class="p-contactArea__card-free-call">
           <div class="p-contactArea__card-free-call-icon">
-          <img width="82" height="62" decoding="async" loading="lazy" src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/icon_free_call.svg' ); ?>" alt="">
+            <img width="82" height="62" decoding="async" loading="lazy" src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/icon_free_call.svg'); ?>" alt="">
           </div>
-          <p class="p-contactArea__card-free-call-text">＊携帯・自動車電話からでもご利用になれます。</p>
+          <div class="p-contactArea__card-free-call-wrapper">
+            <a href="tel:0120-328-049" class="p-contactArea__card-free-call-link u-trim-leading">0120-328-049</a>
+            <p class="p-contactArea__card-free-call-text u-trim-leading">＊携帯・自動車電話からでもご利用になれます。</p>
+          </div>
+        </div>
+      </div>
+      <div class="p-contactArea__card">
+        <p class="p-contactArea__card-header">八幡市にお住まいの方</p>
+        <a href="tel:075-981-8401" class="p-contactArea__card-tel">075-981-8401</a>
+        <div class="p-contactArea__card-free-call">
+          <div class="p-contactArea__card-free-call-icon">
+            <img width="82" height="62" decoding="async" loading="lazy" src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/icon_free_call.svg'); ?>" alt="">
+          </div>
+          <div class="p-contactArea__card-free-call-wrapper">
+            <a href="tel:0120-01-8401" class="p-contactArea__card-free-call-link u-trim-leading">0120-01-8401</a>
+            <p class="p-contactArea__card-free-call-text u-trim-leading">＊携帯・自動車電話からでもご利用になれます。</p>
+          </div>
+        </div>
+      </div>
+      <div class="p-contactArea__card">
+        <p class="p-contactArea__card-header">池田市にお住まいの方</p>
+        <a href="tel:072-751-1100" class="p-contactArea__card-tel">072-751-1100</a>
+        <div class="p-contactArea__card-free-call">
+          <div class="p-contactArea__card-free-call-icon">
+            <img width="82" height="62" decoding="async" loading="lazy" src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/icon_free_call.svg'); ?>" alt="">
+          </div>
+          <div class="p-contactArea__card-free-call-wrapper">
+            <a href="tel:0120-099-328" class="p-contactArea__card-free-call-link u-trim-leading">0120-099-328</a>
+            <p class="p-contactArea__card-free-call-text u-trim-leading">＊携帯・自動車電話からでもご利用になれます。</p>
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
+</section>
